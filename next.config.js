09d5a40c3d60bce351fb26cd03fef1883e47a67d
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-07-28 12:09:11 +0500

    Adding project files

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
